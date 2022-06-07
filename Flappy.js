eae6e67649ea9c290ed3d16ab2128842c75edddf
Rico250 <rico.schlegel@hs-furtwangen.de>
2022-06-07 17:32:56 +0200

    Abgabe 04

--- Flappy.js
+++ Flappy.js
@@ -64,3 +64,14 @@ function draw(){
 }
 
 draw();
+async function sendJSONStringWithPOST(url, jsonString) {
+    const response = await fetch(url, {
+      method: "post",
+      body: jsonString,
+    });
+  }
+
+  sendJSONStringWithPOST(
+    "http://localhost:3000/",
+    JSON.stringify({ test: "Dies ist ein Test" })
+  );
